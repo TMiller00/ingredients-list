77a06e895504fcc96a11cda7f7e2fff4aaeded69
TMiller00 <thomas.d.miller.jr@gmail.com>
2020-02-07 15:48:30 -0800

    Add image assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import brain_health from './brain_health.svg'
+import dna_methylation from './dna_methylation.svg'
+import mood from './mood.svg'
+
+const IconMap = {
+  brain_health: brain_health,
+  dna_methylation: dna_methylation,
+  mood: mood
+}
+
+export default IconMap
