02acbac71faa47b143cb7ab603ff99add6748e2c
TMiller00 <thomas.d.miller.jr@gmail.com>
2020-02-09 11:46:53 -0800

    Detail Page css updates

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Props as IngredientType } from './Ingredient'
 import Icon from './Icon'
 
@@ -11,15 +11,29 @@ type Props = IngredientType & {
   foundIn: string
 }
 
+const ShortBorder = css`
+  content: '';
+  background: #3B4C7F;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 1px;
+  width: 268px;
+`
+
 const Wrapper = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  padding: 0 32px;
+  margin: 28px 32px;
+  width: 580px;
 `
 
 const Header = styled.div`
   display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 266px;
 `
 
 const Title = styled.div`
@@ -27,29 +41,44 @@ const Title = styled.div`
   color: #142B6F;
   letter-spacing: 0;
   line-height: 32px;
+  margin-bottom: 16px;
+  z-index: 1;
 `
 
 const Image = styled.img`
+  position: absolute;
   max-width: 408px;
   width: 100%;
+  right: 0;
 `
 
-const Text = styled.div<{ caps?: boolean }>`
+const Text = styled.div<{ caps?: boolean, thin?: boolean }>`
   font-size: 24px;
   color: #3B4C7F;
   letter-spacing: 0.58px;
   text-transform: ${props => props.caps ? 'uppercase' : 'capitalize' };
+  font-weight: ${props => props.thin ? 300 : 400 };
+  z-index: 1;
 `
 
 const Benefits = styled.div`
   box-sizing: border-box;
   display: flex;
+  position: relative;
   height: 195px;
   align-items: center;
   justify-content: center;
-  border-top: 1px solid black;
-  border-bottom: 1px solid black;
   padding: 32px 0;
+  margin: 20px 0 35px;
+
+  &:before {
+    ${ShortBorder}
+    top: 0;
+  }
+
+  &:after {
+    ${ShortBorder}
+  }
 `
 
 const Benefit = styled.div`
@@ -60,6 +89,10 @@ const Benefit = styled.div`
   height: 100%;
   width: 136px;
   text-align: center;
+
+  img {
+    margin-bottom: 24px;
+  }
 `
 
 const Detail: React.FC<Props> = (props) => {
@@ -68,16 +101,14 @@ const Detail: React.FC<Props> = (props) => {
   return (
     <Wrapper>
       <Header>
-        <div>
-          <Title>{ ingredient }</Title>
-          <Text>{ dosage }</Text>
-        </div>
+        <Title>{ ingredient }</Title>
+        <Text>{ dosage }</Text>
         <Image src={ image } alt={ingredient}/>
       </Header>
-      <Text>FORM: { form }</Text>
-      <Text caps>DOSAGE: { dosage }</Text>
-      <Text>SOURCE: { origin }</Text>
-      <Text>MANUFACTURER: { manufacturer }</Text>
+      <Text caps thin>FORM: { form }</Text>
+      <Text caps thin>DOSAGE: { dosage }</Text>
+      <Text thin>SOURCE: { origin }</Text>
+      <Text thin>MANUFACTURER: { manufacturer }</Text>
       <Benefits>
       { benefits?.map((c: string, i: number) => (
         <Benefit key={i}>
